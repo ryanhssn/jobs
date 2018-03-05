353596435d907f03e7f3ef06b84ccc722f5a2cc7
Bilal H <hussain.bilal000786@gmail.com>
2018-03-05 23:33:26 +0530

    Added search action creator and search button

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
+import { Button } from 'react-native-elements';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
 
 class MapScreen extends Component {
   state = {
@@ -17,10 +21,13 @@ class MapScreen extends Component {
     this.setState({ mapLoaded: true })
   }
 
-  //4201738803816157
 
   onRegionChangeComplete = (region) => {
     this.setState({ region });
+  }
+
+  onButtonPress = () => {
+    this.props.fetchJobs(this.state.region);
   }
 
   render() {
@@ -39,9 +46,27 @@ class MapScreen extends Component {
              region={this.state.region}
              onRegionChangeComplete={this.onRegionChangeComplete}
        />
+        <View style={styles.buttonContainer}>
+          <Button
+            large
+            title="Search This Area"
+            backgroundColor="#009688"
+            icon={{ name: 'search' }}
+            onPress={this.onButtonPress}
+           />
+        </View>
       </View>
     )
   }
 }
 
-export default MapScreen;
+const styles = {
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0
+  }
+}
+
+export default connect(null, actions)(MapScreen);
